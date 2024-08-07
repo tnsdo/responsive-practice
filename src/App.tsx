adfda79a828e29f221831fc91903b6376fc0cce0
tnsdo <jungeun2115@gmail.com>
2024-08-07 20:35:25 +0900

    no res

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ const Back = styled.div`
 
 const Frame5 = styled.div`
   text-align: center;
-  font-size: large;
+  font-size: 18px;
   font-weight: 500;
   color: white;
   display: flex;
@@ -72,14 +72,13 @@ const Frame5 = styled.div`
 
 const Section1 = styled.div`
   width: 400px;
-  height: 400px;
+  height: 340px;
   position: relative;
 `;
 
 const Frame6 = styled.div`
   width: 360px;
-  height: 330px;
-  margin: auto;
+  height: 300px;
   position: absolute;
   top: 50%;
   left: 50%;
@@ -98,7 +97,7 @@ const Frame7 = styled.div`
   float: left;
   margin: 20px;
   flex-direction: row;
-  font-size: larger;
+  font-size: 20px;
   font-weight: 600;
 `;
 
@@ -118,6 +117,7 @@ const Frame9 = styled.div`
   height: 60px;
   align-items: center;
   display: flex;
+  justify-content: flex-start;
 `;
 
 const Frame10 = styled.div`
@@ -131,30 +131,155 @@ const Frame10 = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-left: 20px;
+  margin-right: 10px;
 `;
 
 const Tofu = styled.div`
-  float: left;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
-  margin: 0 15px;
+  margin-right: 100px;
 `;
 
 const Frame11 = styled.div`
   color: gray;
+  font-size: 13px;
 `;
 
 const Frame12 = styled.div`
   color: black;
+  font-size: 20px;
+  font-weight: 700;
 `;
 
 const Frame13 = styled.div`
+  display: flex;
   float: right;
-  background-color: gray;
-  padding: 5px 10px;
+  color: #858585;
+  font-size: 13px;
+  background-color: #f2f4f6;
+  padding: 8px 16px;
   border-radius: 5px;
+`;
+
+const Frame14 = styled.div`
+  float: left;
+  color: white;
+  font-size: 20px;
+  background-color: #2d65f3;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 20px;
+  margin-right: 10px;
+`;
+
+const Frame15 = styled.div`
+  display: flex;
+  float: right;
+  color: #858585;
+  font-size: 13px;
+  background-color: #f2f4f6;
+  padding: 8px 16px;
+  border-radius: 5px;
+  margin-left: 58px;
+`;
+
+const Line = styled.div`
+  width: 360px;
+  height: 40px;
+  align-items: center;
+  display: flex;
+  position: relative;
+`;
+
+const Frame16 = styled.div`
+  width: 90%;
+  height: 1px;
+  background-color: #f2f4f6;
+  display: flex;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const Frame17 = styled.div`
+  width: 360px;
+  height: 60px;
+  align-items: center;
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const Frame18 = styled.div`
+  float: left;
+  color: white;
+  font-size: 20px;
+  font-weight: 700;
+  background-color: #f8c440;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 20px;
+  margin-right: 10px;
+`;
+
+const Frame19 = styled.div`
+  width: 400px;
+  height: 165px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Frame20 = styled.div`
+  width: 120px;
+  height: 150px;
+  position: absolute;
+  background-color: white;
+  border-radius: 15px;
+  align-items: center;
+  margin-left: 20px;
+  align-items: center;
+`;
+
+const Frame21 = styled.div`
+  margin: 20px;
+`;
+
+const Frame22 = styled.div`
+  color: gray;
+  font-weight: 500;
+  font-size: 10px;
+  margin-bottom: 2px;
+`;
+
+const Frame23 = styled.div`
+  color: black;
+  font-weight: 600;
+  font-size: 17px;
+  margin-bottom: 2px;
+`;
+
+const Frame24 = styled.div`
+  color: black;
+  font-weight: 600;
+  font-size: 17px;
+`;
+
+const Frame25 = styled.div`
+  font-size: 45px;
+  position: absolute;
+  right: 10px;
+  bottom: 3px;
 `;
 
 const App = () => {
@@ -193,9 +318,45 @@ const App = () => {
               </Tofu>
               <Frame13>송금</Frame13>
             </Frame9>
+            <Frame9>
+              <Frame14>$</Frame14>
+              <Tofu>
+                <Frame11>두부머니</Frame11>
+                <Frame12>800원</Frame12>
+              </Tofu>
+              <Frame15>송금</Frame15>
+            </Frame9>
           </Money>
+          <Line>
+            <Frame16 />
+          </Line>
+          <Frame17>
+            <Frame18>?</Frame18>
+            <Tofu>
+              <Frame11>공인중개사 합격은</Frame11>
+              <Frame12>에듀윌</Frame12>
+            </Tofu>
+          </Frame17>
         </Frame6>
       </Section1>
+      <Frame19>
+        <Frame20>
+          <Frame21>
+            <Frame22>내 자산</Frame22>
+            <Frame23>두부자산</Frame23>
+            <Frame24>취합해 보기</Frame24>
+          </Frame21>
+          <Frame25>💰</Frame25>
+        </Frame20>
+        <Frame20>
+          <Frame21>
+            <Frame22>내 자산 아님</Frame22>
+            <Frame23>두부자산</Frame23>
+            <Frame24>취합해 보기</Frame24>
+          </Frame21>
+          <Frame25>💵</Frame25>
+        </Frame20>
+      </Frame19>
     </Display>
   );
 };
